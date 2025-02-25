f7da952ed401aaba13853a3fcf762da7bae26294
Azmain Abid Khan <azmain878@gmail.com>
2025-02-25 13:20:24 +0600

    Create code.py

---
+++ binary search/code.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        left=0
+        right=n-1
+        mid=(right+left+1)//2
+        
+        while left<=right:
+            mid=(right+left+1)//2
+            if target==nums[mid]:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+                
+            else:
+                right=mid-1
+           
+        return -1
