42ef51c46e2bd132e127edf2ef9ed2ec6845429a
Azmain Abid Khan <azmain878@gmail.com>
2025-03-18 13:52:42 +0600

    Create code.py

---
+++ Combinations/code.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        sol,res=[],[]
+        def backtrack(x):
+            if len(sol)==k:
+                res.append(sol[:])
+                return
+            left=x
+            still_need=k-len(sol)
+            if left>still_need:
+                backtrack(x-1)
+            sol.append(x)
+            backtrack(x-1)
+            sol.pop()
+        backtrack(n)
+        return res
