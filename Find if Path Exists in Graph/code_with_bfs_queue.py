7831d59bc008005bdcf9605acfb22d1f3527e6aa
Azmain Abid Khan <azmain878@gmail.com>
2025-03-18 20:55:14 +0600

    Create code_with_bfs_queue.py

---
+++ Find if Path Exists in Graph/code_with_bfs_queue.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source==destination:
+            return True
+        graph=defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        seen=set()
+        seen.add(source)
+        q=deque()
+        q.append(source)
+
+        while q:
+            node=q.popleft()
+            if node==destination:
+                return True
+            for nei in graph[node]:
+                if nei not in seen:
+                    seen.add(nei)
+                    q.append(nei)
+        return False
