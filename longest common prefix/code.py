ca1b29955032778702b4528d6bfa2f25a3e89d8a
Azmain Abid Khan <azmain878@gmail.com>
2025-03-30 00:59:12 +0600

    Update code.py

--- longest common prefix/code.py
+++ longest common prefix/code.py
@@ -1,9 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix=strs[0]
-        for i in range(1,len(strs)):
-            while not strs[i].startswith(prefix):
-                prefix=prefix[:-1]
-                if not prefix:
-                    return ""
-        return prefix
+        min_length = float("inf")
+        for s in strs:
+            if len(s) < min_length:
+                min_length = len(s)
+
+        i = 0
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i += 1
+
+        return s[:i]
